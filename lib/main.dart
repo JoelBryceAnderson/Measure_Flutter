3a5130e5573ffe111ac40c3ecb975274010c89ad
Joel Anderson <joelbryceanderson@gmail.com>
2018-04-29 11:57:01 -0400

    added image asset, loaded into view

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: new MyHomePage(),
+      home: new RulerPage(),
     );
   }
 
@@ -23,50 +23,52 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
+class RulerPage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _RulerPageState createState() => new _RulerPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
+class _RulerPageState extends State<RulerPage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'Hello world!',
+      body: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          new Expanded(
+              child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Text(
+                'Hello world!',
+              ),
+            ],
+          )),
+          new Expanded(
+              child: new Container(
+            color: Colors.green,
+            child: new Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Image(
+                    height: 48.0,
+                    width: 48.0,
+                    image: new AssetImage('assets/ruler.png')),
+                new Text(
+                  'Measure',
+                  style: new TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      fontSize: 24.0),
+                ),
+                new Text(
+                  'Made by Joel Anderson',
+                  style: new TextStyle(color: Colors.white),
+                ),
+              ],
             ),
-          ],
-        ),
+          ))
+        ],
       ),
     );
   }
